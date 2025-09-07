3cdb1d84b9709349c26b516af996914e166a8dd8
ucaeon <yoocw0426@gmail.com>
2025-09-07 21:23:40 +0900

    Initial project setup

---
+++ src/main/java/com/ucaeon/yumbook/YumbookApplication.java
@@ -0,0 +1,13 @@
+package com.ucaeon.yumbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucaeon/yumbook/YumbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucaeon.yumbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
